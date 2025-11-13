3c7f8964300bdcb28b9a874cbe1be3541d935942
sylvia <lisiyansylvia@gmail.com>
2025-11-13 13:18:17 -0500

    Adding requirements, updating sbatch

--- run_opt_scripts.sh
+++ run_opt_scripts.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+cd /ocean/projects/cis250134p/shared/div_data_pupa
+module load anaconda3
+conda activate pupa
 python optimization_script.py --num_iters 3
 python optimization_script.py --num_iters 3 --optimizer simba
